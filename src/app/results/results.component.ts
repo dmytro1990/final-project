da3f9b281d6f0902c1f4fb4144db75ec59c38027
“Patrick <ladukepatrick@gmail.com>
2019-12-04 20:47:41 -0500

    third commit

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../search/search.service';
-
+declare global {
+	interface Window {
+		google: any;
+	}
+}
 
 @Component({
   selector: 'app-results',
@@ -8,14 +12,24 @@ import { SearchService } from '../search/search.service';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
-
  airPosts: any[] = [];
-  
-
-
+ initMap () {
+  var  map  =  new  window.google.maps.Map(document.getElementById('map'),  {  
+    center:  new  window.google.maps.LatLng(51.505,  -0.09),  
+    mapTypeId:  window.google.maps.MapTypeId.ROADMAP,  
+    zoom:  11  
+});  
+  var  t  =  new  Date().getTime();  
+  var  waqiMapOverlay  =  new  window.google.maps.ImageMapType({  
+        getTileUrl:  function(coord,  zoom)  {  
+                  return  'https://tiles.waqi.info/tiles/usepa-aqi/'  +  zoom  +  "/"  +  coord.x  +  "/"  +  coord.y  +  ".png?token=6093a570588d1a52e24bdde7f35550c7486d2fd8";  
+        },  
+        name:  "Air  Quality",  
+  });  
+  map.overlayMapTypes.insertAt(0,waqiMapOverlay);  
+}
 // step 8 - set up a constructor
   constructor(private searchService: SearchService) { }
-
   search(subName: string): void {
 	  console.log(this.searchService);
 // step 9 - call the services's method that makes the request    
@@ -29,10 +43,8 @@ export class ResultsComponent implements OnInit {
     }, error => {
       console.log(`oh  no. ${error}`);
     });
-
   }
-
   ngOnInit() {
-    
+    this.initMap();
   }
 }
